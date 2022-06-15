c924b4885a173f00e60443614132dd32b5e95bad
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-06-14 19:01:28 -0700

    Initial commit

---
+++ server/dev-middleware.js
@@ -0,0 +1,16 @@
+module.exports = function devMiddleware(publicPath) {
+  const livereload = require('livereload').createServer();
+  livereload.server.once('connection', () => {
+    setTimeout(() => livereload.sendAllClients(JSON.stringify({
+      command: 'reload',
+      path: '/'
+    })), 100);
+  });
+  livereload.watch(publicPath);
+  const webpack = require('webpack')(require('../webpack.config'));
+  return [
+    require('connect-livereload')(),
+    require('webpack-dev-middleware')(webpack),
+    require('webpack-hot-middleware')(webpack)
+  ];
+};
