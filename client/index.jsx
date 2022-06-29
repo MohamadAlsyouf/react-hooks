35f9928ab0d7189d3e0fa9fb01972bb70df4ecd6
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-06-28 23:13:49 -0700

    Created individual files for hooks examples for easy reference

--- client/index.jsx
+++ client/index.jsx
@@ -1,44 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Checkbox from './checkbox';
+import ShowStarRating from './star-rating';
 import { createRoot } from 'react-dom/client';
 import '../server/public/styles.css';
-import { FaStar } from 'react-icons/fa';
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// helper function to create an array of stars
-const createArray = length => [
-  ...Array(length)
-];
-
-function Star({ selected = false, onSelect }) {
-  return <FaStar color={selected ? 'red' : 'gray'}
-  onClick={onSelect}/>;
-}
-
-function StarRating({ totalStars = 5 }) {
-  const [
-    selectedStars,
-    setSelectedStars
-  ] = useState(0);
-
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)}
-        />
-      ))}
-    <p>{selectedStars} of {totalStars}</p>
-    </>
-  );
-}
-
-function App() {
-  return <StarRating totalStars={5}/>;
-}
-
 root.render(
   <React.StrictMode>
-    <App />
+    <Checkbox />
+    <ShowStarRating />
   </React.StrictMode>
 );
