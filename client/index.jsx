8c24f89f5c25d2a29e40cf98f9b1184085dfa6fd
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-06-30 19:36:02 -0700

    Completed useEffect hook introduction

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Checkbox from './checkbox';
 import ShowStarRating from './star-rating';
+import UseEffect from './useEffect-intro';
 import { createRoot } from 'react-dom/client';
 import '../server/public/styles.css';
 const container = document.getElementById('root');
@@ -10,5 +11,6 @@ root.render(
   <React.StrictMode>
     <Checkbox />
     <ShowStarRating />
+    <UseEffect />
   </React.StrictMode>
 );
